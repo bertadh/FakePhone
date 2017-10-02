a2f2227ca47798150e0f99064f8d5863d039edca
bertadh <bertadomingo1@hotmail.com>
2017-10-02 16:57:28 +0200

    Versio final primera

--- app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
@@ -5,8 +5,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FakePhoneActivity extends AppCompatActivity {
+
+    EditText edit_num = (EditText) findViewById(R.id.edit_num);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +20,28 @@ public class FakePhoneActivity extends AppCompatActivity {
 
     public void clicat (View v){
         Button boto = (Button) v;
+
+        String num = edit_num.getText().toString() + boto.getId();
+        edit_num.setText(num);
+
         Log.i("click","Has clicat el boto!");
         //TODO: Metode que escrigui numeros per pantalla
+    }
+
+    public void trucada (View v){
+        String trucant = String.format("Trucant al ", edit_num.getText());
+        Toast.makeText(FakePhoneActivity.this, trucant, Toast.LENGTH_SHORT).show();
+        finish();
+
+        //Log.i("click","Has clicat el boto TRUCADA!");
+        //TODO: Afegir un toast que em mostri el numero que hi ha al "id_numero"
+    }
+
+    public void esborra (View v){
+        String num = " ";
+        edit_num.setText(num);
+
+        //Log.i("click","Has clicat el boto ESBORRA!");
+        //TODO: Esborrar tot el text que hi hagui en el "id_numero"
     }
 }
